e7ece4be555256c02ca2fe3d27e2bbe9a05cc45e
Dakshina Dissanayake <i4matrix.dakshina@gmail.com>
2026-05-20 21:11:57 +0530

    temp: hardcode db url to test connection

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,8 @@
-import dotenv from "dotenv";
 import { defineConfig } from "prisma/config";
-
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: ".env.local" });
-}
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL ?? "postgresql://postgres:StayDB2026!@serene-stay-db.c3me0aw0uxob.ap-south-1.rds.amazonaws.com:5432/devops_aws",
   },
 });
